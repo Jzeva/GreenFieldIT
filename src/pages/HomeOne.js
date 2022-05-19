a623de16687c14d67c8576a38618fb072d764e53
Jzeva <jzeva1213@gmail.com>
2022-05-19 13:40:45 +1000

    add icon and title for the website

--- src/pages/HomeOne.js
+++ src/pages/HomeOne.js
@@ -17,7 +17,7 @@ import ScrollToTop from '../components/ScrollToTop.jsx';
 const HomeOne = () => {
     return (
         <React.Fragment>
-            <SEO title="Exomac – Business React JS Template" />
+            <SEO title="Greenfield IT" />
             <Header />
             <IntroSlider />
             <HomeAbout />
