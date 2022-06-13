0d99753e143b7b5d9739061da16c287080d68190
Jzeva <jzeva1213@gmail.com>
2022-06-13 10:40:37 +1000

    change about us page

--- src/container/About/AboutFour.js
+++ src/container/About/AboutFour.js
@@ -1,42 +1,59 @@
-import {useState, useEffect, useRef} from "react";
-import SectionTitleTwo from '../../components/SectionTitles/SectionTitleTwo';
-import Tilt from 'react-parallax-tilt';
-import Parallax from 'parallax-js';
-import CountUp from 'react-countup';
-import VisibilitySensor from "react-visibility-sensor";
+import { useState, useEffect, useRef } from "react";
+import SectionTitleTwo from "../../components/SectionTitles/SectionTitleTwo";
+import Tilt from "react-parallax-tilt";
+import Parallax from "parallax-js";
+import { Link } from "react-router-dom";
+// import CountUp from 'react-countup';
+// import VisibilitySensor from "react-visibility-sensor";
 
 const AboutFour = () => {
-    const [didViewCountUp, setDidViewCountUp] = useState(false);
-    const onVisibilityChange = isVisible => {
-        if (isVisible) {
-        setDidViewCountUp(true);
-        }
-    };
-    const [scale] = useState(1.04);
-    const sceneEl = useRef(null);
-    useEffect(() => {
-        const parallaxInstance = new Parallax(sceneEl.current, {
-        relativeInput: true,
-        })
-        
-        parallaxInstance.enable();
+  // const [didViewCountUp, setDidViewCountUp] = useState(false);
+  // const onVisibilityChange = isVisible => {
+  //     if (isVisible) {
+  //     setDidViewCountUp(true);
+  //     }
+  // };
+  const [scale] = useState(1.04);
+  const sceneEl = useRef(null);
+  useEffect(() => {
+    const parallaxInstance = new Parallax(sceneEl.current, {
+      relativeInput: true,
+    });
 
-        return () => parallaxInstance.disable();
+    parallaxInstance.enable();
 
-    }, [])
-    return (
-        <div className="section section-padding-top about-section-padding-bottom-200">
-            <div className="container">
-                <div className="row">
+    return () => parallaxInstance.disable();
+  }, []);
+  return (
+    <div className="section section-padding-top about-section-padding-bottom-180">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up">
+            <div className="about-content-area mt-0 mb-md-10 mb-10">
+              <SectionTitleTwo
+                subTitle="Web design and digital marketing"
+                title="We think strategy, UX design, and web development"
+              />
+              <p>
+                As a business owner, you face a whole raft of challenges – to do
+                more in less time, to grow your business despite stiff
+                competition at every point, to operate optimally within a
+                limited budget, to keep your employees and customers happy
+                without sacrificing your sanity and to maintain your business
+                ethics in an increasingly competitive environment. The list goes
+                on – but having to deal with IT headaches shouldn’t be on it.
+                You, and your business, need cost-effective and efficient
+                technology solutions so that you can concentrate on the things
+                you do well – the core concerns of your business.
+              </p>
+              <Link
+                className="btn btn-primary btn-hover-secondary mt-xl-12 mt-lg-8 mt-md-6 mt-4"
+                to={process.env.PUBLIC_URL + "/"}
+              >
+                Get Started
+              </Link>
 
-                    <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up">
-                        <div className="about-content-area mt-0 mb-md-10 mb-10">
-                            <SectionTitleTwo 
-                                subTitle="Web design and digital marketing"
-                                title="We think strategy, UX design, and web development"
-                            />
-
-                            <div className="row row-cols-sm-2 row-cols-auto mb-n6">
+              {/* <div className="row row-cols-sm-2 row-cols-auto mb-n6">
                                 <div className="col mb-6">
                                     <div className="about-funfact">
                                         <div className="number">
@@ -67,33 +84,52 @@ const AboutFour = () => {
                                         <p>We help our clients increase profits by increasing their visibility online.</p>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-xl-6 col-lg-6 col-12" data-aos="fade-up" data-aos-delay="300">
-                        <div className="about-image-area about-shape-animation right-0 mr-0">
-                            <div className="about-image js-tilt">
-                                <Tilt scale={scale} transitionSpeed={4000}>
-                                    <img src={process.env.PUBLIC_URL + "images/about/about-1.jpg"} alt="" />
-                                </Tilt>
-                            </div>
-                            <div className="about-image js-tilt">
-                                <Tilt scale={scale} transitionSpeed={4000}>
-                                    <img src={process.env.PUBLIC_URL + "images/about/about-2.jpg"} alt="" />
-                                </Tilt>
-                            </div>
-                            <div className="shape shape-1" id="scene" ref={sceneEl}>
-                                <span data-depth="1"><img src={process.env.PUBLIC_URL + "images/shape-animation/video-shape-1.png"} alt="" /></span>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+                            </div> */}
             </div>
+          </div>
+
+          <div
+            className="col-xl-6 col-lg-6 col-12"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <div className="about-image-area about-shape-animation right-0 mr-0">
+              <div className="about-image js-tilt">
+                <Tilt scale={scale} transitionSpeed={4000}>
+                  <img
+                    src={process.env.PUBLIC_URL + "images/about/tirza-van.jpg"}
+                    alt=""
+                  />
+                </Tilt>
+              </div>
+              <div className="about-image js-tilt">
+                <Tilt scale={scale} transitionSpeed={4000}>
+                  <img
+                    src={
+                      process.env.PUBLIC_URL +
+                      "images/about/thisisengineering-raeng.jpg"
+                    }
+                    alt=""
+                  />
+                </Tilt>
+              </div>
+              <div className="shape shape-1" id="scene" ref={sceneEl}>
+                <span data-depth="1">
+                  <img
+                    src={
+                      process.env.PUBLIC_URL +
+                      "images/shape-animation/video-shape-1.png"
+                    }
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default AboutFour;
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,31 +3,26 @@ import SEO from '../components/SEO';
 import Header from "../partials/header/Header";
 import Breadcrumb from '../container/Breadcrumb/Breadcrumb';
 import AboutFour from '../container/About/AboutFour';
-import Video from '../container/Video/Video';
+// import Video from '../container/Video/Video';
 import AboutFive from '../container/About/AboutFive';
 import TestimonialContainer from '../container/Testimonial/TestimonialContainer';
 import CallToActionTwo from '../container/CallToAction/CallToActionTwo';
 import Footer from '../container/Footer/Footer';
 import ScrollToTop from '../components/ScrollToTop.jsx';
 
-
-
-
-
-
 const AboutUs = () => {
     return (
         <React.Fragment>
-            <SEO title="Exomac || About" />
+            <SEO title="GreenfieldIT || About" />
             <Header />
             <Breadcrumb 
-                image="images/bg/breadcrumb-bg.jpg"
-                title="We are an agency located in New York"
+                image="images/bg/breadcrumb-bg1.jpg"
+                title="We are a company located in Brisbane"
                 content="Home"
                 contentTwo="About Us"
             />
             <AboutFour />
-            <Video />
+            {/* <Video /> */}
             <AboutFive />
             <TestimonialContainer classOption="bg-primary-blue" />
             <CallToActionTwo />
