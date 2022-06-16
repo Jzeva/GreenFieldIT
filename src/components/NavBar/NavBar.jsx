099a82f7763c53a2468ec439bee8fb0964671b93
Jzeva <jzeva1213@gmail.com>
2022-06-16 10:16:28 +1000

    redesign the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,76 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <nav className="site-main-menu">
-            <ul>
-                {/* <li>
+  return (
+    <nav className="site-main-menu">
+      <ul>
+        {/* <li>
                     <NavLink to={process.env.PUBLIC_URL + "/home-one"}><span className="menu-text">Homepage</span></NavLink> */}
-                    {/* <span className="menu-toggle"><i className="far fa-angle-down"></i></span>
+        {/* <span className="menu-toggle"><i className="far fa-angle-down"></i></span>
                     <ul className="sub-menu">
                         <li><NavLink to={process.env.PUBLIC_URL + "/home-one"}><span className="menu-text">Home One</span></NavLink></li>
                         <li><NavLink to={process.env.PUBLIC_URL + "/home-two"}><span className="menu-text">Home Two</span></NavLink></li>
                         <li><NavLink to={process.env.PUBLIC_URL + "/home-three"}><span className="menu-text">Home Three</span></NavLink></li>
                     </ul> */}
-                {/* </li> */}
-                <li>
-                    <NavLink to={process.env.PUBLIC_URL + "/about"}><span className="menu-text">About Us</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to={process.env.PUBLIC_URL + "/service"}><span className="menu-text">Managed Services</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to={process.env.PUBLIC_URL + "/service"}><span className="menu-text"> Business Continuity</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to={process.env.PUBLIC_URL + "/service"}><span className="menu-text">IT Security</span></NavLink>
-                </li>
-                <li>
-                    <NavLink to={process.env.PUBLIC_URL + "/service"}><span className="menu-text"> Office 365</span></NavLink>
-                </li>
+        {/* </li> */}
+        <li>
+          <NavLink to={process.env.PUBLIC_URL + "/about"}>
+            <span className="menu-text">About Us</span>
+          </NavLink>
+        </li>
+        {/* <li>
+          <NavLink to={process.env.PUBLIC_URL + "/service"}>
+            <span className="menu-text">Managed Services</span>
+          </NavLink>
+        </li> */}
+        {/* <li>
+          <NavLink to={process.env.PUBLIC_URL + "/service"}>
+            <span className="menu-text"> Business Continuity</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={process.env.PUBLIC_URL + "/service"}>
+            <span className="menu-text">IT Security</span>
+          </NavLink>
+        </li> */}
+        {/* <li>
+          <NavLink to={process.env.PUBLIC_URL + "/service"}>
+            <span className="menu-text"> Office 365</span>
+          </NavLink>
+        </li> */}
+        <li className="has-children">
+          <NavLink to={process.env.PUBLIC_URL + "/home-one"}>
+            <span className="menu-text">Services</span>
+          </NavLink>{" "}
+          <span className="menu-toggle">
+            <i className="far fa-angle-down"></i>
+          </span>
+          <ul className="sub-menu">
+            <li>
+              <NavLink to={process.env.PUBLIC_URL + "/work-details/1"}>
+                <span className="menu-text">Managed Services</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={process.env.PUBLIC_URL + "/home-two"}>
+                <span className="menu-text">Business Continuity</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={process.env.PUBLIC_URL + "/home-three"}>
+                <span className="menu-text">IT Security</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={process.env.PUBLIC_URL + "/home-three"}>
+                <span className="menu-text">Office 365</span>
+              </NavLink>
+            </li>
+          </ul>
+        </li>
 
-                {/* <li className="has-children">
+        {/* <li className="has-children">
                     <NavLink to={process.env.PUBLIC_URL + "/work"}><span className="menu-text">Work</span></NavLink>
                     <span className="menu-toggle"><i className="far fa-angle-down"></i></span>
                     <ul className="sub-menu">
@@ -47,12 +87,12 @@ const NavBar = () => {
                         <li><NavLink to={process.env.PUBLIC_URL + `/blog-details/1`}><span className="menu-text">Blog Details</span></NavLink></li>
                     </ul>
                 </li> */}
-                {/* <li>
+        {/* <li>
                     <NavLink to={process.env.PUBLIC_URL + "/contact"}><span className="menu-text">Contact Us</span></NavLink>
                 </li> */}
-            </ul>
-        </nav>
-    )
-}
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
